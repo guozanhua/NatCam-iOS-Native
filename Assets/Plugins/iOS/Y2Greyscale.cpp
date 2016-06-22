cb9a67ad9a1714e5171f26885f8f9b73a23c267a
Memuna Olokoba <maiisalihu@yahoo.com>
2016-06-22 11:37:55 -0400

    Update Y2Greyscale.cpp

--- Assets/Plugins/iOS/Y2Greyscale.cpp
+++ Assets/Plugins/iOS/Y2Greyscale.cpp
@@ -11,13 +11,13 @@ void Convert (void** rgbBuffer, const void* yBuffer, const size_t size) {
     //Null checking
     if (rgb24 == NULL) rgb24 = new byte[size * 3]; //We multiply size by 3 because this is 3-channel RGB24
     //Iterate
-    for (int i = 0; i < size; i++) { //Performing a C-style cast is bad practice, use reinterpret_cast<byte*> instead >:(
+    for (int i = 0, p; i < size; i++, p = i * 3) { //Performing a C-style cast is bad practice, use reinterpret_cast<byte*> instead >:(
         //Set the R channel
-        rgb24[i * 3] = ((byte*)yBuffer)[i];
+        rgb24[p] = ((byte*)yBuffer)[i];
         //Set the G channel
-        rgb24[i * 3 + 1] = ((byte*)yBuffer)[i];
+        rgb24[++p] = ((byte*)yBuffer)[i];
         //Set the B channel
-        rgb24[i * 3 + 2] = ((byte*)yBuffer)[i];
+        rgb24[++p] = ((byte*)yBuffer)[i];
     }
     //Assign the RGB buffer pointer
     *rgbBuffer = (void*)rgb24;
